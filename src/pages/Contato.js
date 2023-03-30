6e3dc73049452a489e37754928259b9affdbd159
Luc-coder <lucasmarquesrussi8@gmail.com>
2023-03-30 16:42:59 -0300

    fazer skill e home

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -1,10 +1,25 @@
+// import { BsGithub } from 'react-icons/bs'
+
 import styles from '../components/css/Contato.module.css'
 
 function Contato(){
 	return(
 		<div className={styles.main}>
 			<div className={styles.container}>
-				<p>teste Contato</p>
+				<div className={styles.mainBox}>
+					<div className={styles.singleBox}>
+						<p>Github: <a href='https://github.com/Luc-coder'>https://github.com/Luc-coder</a></p>
+					</div>
+					<div className={styles.singleBox}>
+						<p>Linkedin: <a href='https://www.linkedin.com/in/lucas-marques-russi/'>https://www.linkedin.com/in/lucas-marques-russi/</a></p>
+					</div>
+					<div className={styles.singleBox}>
+						<p>Email: lucasmarquesrussi8@gmail.com</p>
+					</div>
+					<div className={styles.singleBox}>
+						<p>Tel: (11) 97951-1800</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -5,13 +5,13 @@ function Sobre(){
 		<div className={styles.main}>
 			<div className={styles.container}>
 				<div className={styles.boxText}>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium quam non justo pretium finibus. Nunc quis ipsum fermentum est dictum tincidunt. Quisque dapibus nisi vel elit porta suscipit. Aenean feugiat lorem id dolor semper eleifend. Etiam malesuada massa quis orci cursus laoreet. Sed venenatis semper lacinia. Suspendisse id sapien justo. In gravida, libero vestibulum scelerisque posuere, erat risus condimentum velit, sit amet pretium enim velit ornare neque. Vivamus porttitor vehicula bibendum.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium quam non justo pretium finibus. Nunc quis ipsum fermentum est dictum tincidunt. Quisque dapibus nisi vel elit porta suscipit. Aenean feugiat lorem id dolor semper eleifend.</p>
 				</div>
 				<div className={styles.boxText}>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium quam non justo pretium finibus. Nunc quis ipsum fermentum est dictum tincidunt. Quisque dapibus nisi vel elit porta suscipit. Aenean feugiat lorem id dolor semper eleifend. Etiam malesuada massa quis orci cursus laoreet. Sed venenatis semper lacinia. Suspendisse id sapien justo. In gravida, libero vestibulum scelerisque posuere, erat risus condimentum velit, sit amet pretium enim velit ornare neque. Vivamus porttitor vehicula bibendum.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium quam non justo pretium finibus. Nunc quis ipsum fermentum est dictum tincidunt. Quisque dapibus nisi vel elit porta suscipit. Aenean feugiat lorem id dolor semper eleifend.</p>
 				</div>
 				<div className={styles.boxText}>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium quam non justo pretium finibus. Nunc quis ipsum fermentum est dictum tincidunt. Quisque dapibus nisi vel elit porta suscipit. Aenean feugiat lorem id dolor semper eleifend. Etiam malesuada massa quis orci cursus laoreet. Sed venenatis semper lacinia. Suspendisse id sapien justo. In gravida, libero vestibulum scelerisque posuere, erat risus condimentum velit, sit amet pretium enim velit ornare neque. Vivamus porttitor vehicula bibendum.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pretium quam non justo pretium finibus. Nunc quis ipsum fermentum est dictum tincidunt. Quisque dapibus nisi vel elit porta suscipit. Aenean feugiat lorem id dolor semper eleifend.</p>
 				</div>
 
 				<div className={styles.boxGaleria}>
