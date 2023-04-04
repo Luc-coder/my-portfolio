17be94cb50a198ff3fbc9aaaf233764cb2c3103c
Luc-coder <lucasmarquesrussi8@gmail.com>
2023-04-04 11:49:23 -0300

    home feita

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,26 @@
 import styles from '../components/css/Home.module.css'
 
+import { HiOutlineCode } from 'react-icons/hi'
+
 import '../components/css/root.css'
 
 function Home(){
 	return(
 		<div className={styles.main}>
 			<div className={styles.container}>
-				<p>Home</p>
+				<div className={styles.names}>
+					<div className={styles.fname}><p>Lucas</p></div>
+					<div className={styles.mname}><p>Marques</p></div>
+					<div className={styles.lname}><p>Russi</p></div>
+				</div>
+				<div className={styles.dev}>
+					<div className={styles.text}>
+						<p>WEB<br></br>DEVELOPER</p>
+					</div>
+					<div className={styles.imgBox}>
+						<HiOutlineCode color='white'/>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
